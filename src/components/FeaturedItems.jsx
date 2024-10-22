aca996126982599f9d1f33034222b881073549b8
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-10-23 03:26:50 +0530

    Update

--- src/components/FeaturedItems.jsx
+++ src/components/FeaturedItems.jsx
@@ -11,7 +11,7 @@ const FeaturedItems = () => {
 
     return (
         <div className="container mx-auto py-10">
-            <h2 className="text-3xl text-center ml-7 md:text-start font-bold mb-6">Featured Items</h2>
+            <h2 className="text-3xl text-center md:ml-7 md:text-start font-bold mb-6">Featured Items</h2>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mx-5 gap-6">
                 {featuredMen.concat(featuredWomen).map(item => (
                     <ItemCard key={item.id} item={item} />
