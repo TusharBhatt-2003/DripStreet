bd0db2d797061169666255fce0a65df093cf633b
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-10-23 05:57:33 +0530

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-// Header.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 // Import Font Awesome icons (optional)
@@ -7,7 +6,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
-        <header className="p-4 border-2 mt-1 mx-1 border-black rounded-xl">
+        <header className="sticky top-1 bg-white/30 backdrop-blur-lg p-4 border-2 mt-1 mx-1 border-black rounded-xl z-50">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-black text-2xl font-bold">
                     <Link to="/">DripStreet</Link>
@@ -15,17 +14,17 @@ const Header = () => {
                 <nav>
                     <ul className="flex items-center space-x-4">
                         <li>
-                            <Link to="/men" className="text-black hover:bg-gray-100 px-3 py-2 rounded">
+                            <Link to="/men" className="text-black hover:bg-gray-100/30 hover:backdrop-blur-3xl px-3 py-2 rounded-3xl">
                                 Men
                             </Link>
                         </li>
-                        <li>
-                            <Link to="/women" className="text-black hover:bg-gray-100 px-3 py-2 rounded">
+                        <l3>
+                          <Link to="/women" className="text-black hover:bg-gray-100/30 hover:backdrop-blur-3xl px-3 py-2 rounded-3xl">
                                 Women
                             </Link>
-                        </li>
+                        </l3>
                         <li>
-                            <Link to="/cart" className="text-black hover:bg-gray-100 px-3 py-2 rounded flex items-center">
+                            <Link to="/cart" className="text-black hover:bg-gray-100/30 hover:backdrop-blur-3xl px-3 py-2 rounded-3xl flex items-center">
                                 <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
                                 Cart
                             </Link>
