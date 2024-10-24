03e8fcd6d9d4a839e840f02ba83e9c95c493be9c
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-10-24 21:53:27 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,30 @@ import Cart from './pages/Cart';
 import ItemPage from './pages/ItemPage';
 import Favicon from './components/Favicon'; // Import the Favicon component
 import Payment from './pages/Payment';
-import PaymentConfirmation from './pages/PaymentConfiermation';
+import PaymentConfirmation from './pages/PaymentConfirmation';
 import Footer from './components/Footer';
 import OrderHistory from './pages/OrderHistory';
 
 const App = () => {
     return (
         <Router>
-            <Header />
-            <Favicon /> {/* Favicon inside the Router */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/men" element={<Men />} />
-                <Route path="/women" element={<Women />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/item/:id" element={<ItemPage />} />
-                <Route path='/payment' element={<Payment />} />
-                <Route path='/payment-confirmation' element={<PaymentConfirmation />} />
-                <Route path="/order-history" element={<OrderHistory />} />
-            </Routes>
-            <Footer />
+            <div className="flex flex-col min-h-screen"> {/* Flexbox layout */}
+                <Header />
+                <Favicon /> {/* Favicon inside the Router */}
+                <main className="flex-grow"> {/* Main content area */}
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/men" element={<Men />} />
+                        <Route path="/women" element={<Women />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/item/:id" element={<ItemPage />} />
+                        <Route path="/payment" element={<Payment />} />
+                        <Route path="/payment-confirmation" element={<PaymentConfirmation />} />
+                        <Route path="/order-history" element={<OrderHistory />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
         </Router>
     );
 };
